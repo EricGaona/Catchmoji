e9ff0b637134bc04a1e13e992f598ea66cee0c53
Eric <ericgaona0413@gmail.com>
2022-07-16 20:54:46 +0100

    testing the scripts.js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,17 @@
-var questions = [{"id":1,
+var questions = [
+                {"id":1,
                 "answer":["Like a duck to water","another","another one"],
                 "right":"Like a duck to water",
-                "image": "assets/images/arm-and-leg.png"}];
+                "image": "assets/images/arm-and-leg.png"},
+                {"id":2,
+                "answer":["Like a duck to water_2","another_2","another one_2"],
+                "right":"Like a duck to water_2",
+                "image": "assets/images/burn-bridges.png"},
+                {"id":3,
+                "answer":["Like a duck to water_3","another_3","another one_3"],
+                "right":"Like a duck to water_3",
+                "image": "assets/images/fish-fry.png"},
+            ];
 
 
 function displayQuestion(jsonPreguntas){
